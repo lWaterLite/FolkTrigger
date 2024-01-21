6ebf9fa895aedb2885d3101303bcbf8f0bd5de3a
lWaterLite <lwaterlite@126.com>
2024-01-21 11:57:13 +0800

    feat(SvcPage): Complete link button section for svc page.

--- FolkTrigger/Pages/SvcPage.xaml.cs
+++ FolkTrigger/Pages/SvcPage.xaml.cs
@@ -1,11 +1,69 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace FolkTrigger.Pages;
 
-public partial class SvcPage : Page
+public partial class SvcPage
 {
+    
+    private readonly string _basePath = AppDomain.CurrentDomain.BaseDirectory + @"MetaFolk_SVC\";
+    
     public SvcPage()
     {
         InitializeComponent();
     }
+
+    #region RoutedEventMethods
+
+    private void SvcPageLinkButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is not Button button) return;
+        if (button.Tag is not SvgLink svgLink) return;
+
+        switch (svgLink)
+        {
+            case SvgLink.Folder:
+                Process.Start("explorer", _basePath);
+                break;
+            case SvgLink.Readme:
+                Process.Start("explorer", _basePath + "readme.md");
+                break;
+            case SvgLink.Configs:
+                Process.Start("explorer", _basePath + "configs");
+                break;
+            case SvgLink.DatasetRaw:
+                Process.Start("explorer", _basePath + "dataset_raw");
+                break;
+            case SvgLink.Logs:
+                Process.Start("explorer", _basePath + "logs");
+                break;
+            case SvgLink.Pretrain:
+                Process.Start("explorer", _basePath + "pretrain");
+                break;
+            case SvgLink.Trained:
+                Process.Start("explorer", _basePath + "trained");
+                break;
+            case SvgLink.Results:
+                Process.Start("explorer", _basePath + "results");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(svgLink.ToString());
+        }
+    }
+
+    #endregion
+}
+
+public enum SvgLink
+{
+    Folder,
+    Readme,
+    Configs,
+    DatasetRaw,
+    Logs,
+    Pretrain,
+    Trained,
+    Results
 }
